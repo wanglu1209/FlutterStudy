b815f26bd11bbd424d4181a2ae9f3c158f75d11f
王璐 <2021765485@qq.com>
2018-12-10 20:53:00 +0800

    跳转新页面等

---
+++ lib/book/NewRoute.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NewRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hello'),
+      ),
+      body: Center(
+        child: Text('这是一个新的页面'),
+      ),
+    );
+  }
+}
